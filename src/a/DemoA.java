feef5c2b2a64bfdf5b34df0c957ee5f7c9f19df2
Haithem AlSaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-05 10:26:51 -0500

    Update DemoA.java

--- src/a/DemoA.java
+++ src/a/DemoA.java
@@ -4,7 +4,9 @@ public class SimpleAddition {
 
     public static void main(String[] args) {
         int result = add(5, 7);
+        int result2 = divide (18,6);
         System.out.println("Result: " + result);
+         System.out.println("Result: " + result2);
     }
 
     static int add(int a, int b) {
